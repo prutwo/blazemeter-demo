746802513c2c6b8f9f8bcc45b9fa548503c72866
prutwo <ophir.prusak@blazemeter.con>
2015-02-18 16:26:23 -0500

    added sleep 1 second

--- gtm_inc.php
+++ gtm_inc.php
@@ -7,4 +7,4 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-MW5ZJ6');</script>
 <!-- End Google Tag Manager -->
-
+<? sleep(1); ?>
